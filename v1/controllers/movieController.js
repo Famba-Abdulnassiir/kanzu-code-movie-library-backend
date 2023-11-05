f4b6ff6a94c9edfc4db17c9b649776d2d5d36160
Famba Abdulnassiir Siraji <57795345+FambaAbdulnassiirSiraji@users.noreply.github.com>
2023-11-05 13:45:52 +0300

    add image upload with multer and cloudinary to create movie end point

--- v1/controllers/movieController.js
+++ v1/controllers/movieController.js
@@ -1,6 +1,21 @@
 const {PrismaClient} = require('@prisma/client')
 const prisma = new PrismaClient();
+const dotenv = require('dotenv');
+dotenv.config();
+const multer = require('multer');
+const cloudinary = require('cloudinary');
+const {unlink} = require('fs');
 
+const upload = multer({dest: "uploads/"});
+
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET,
+    shorten: true,
+    secure: true,
+    ssl_detected: true
+})
 
 const getAllMovies = async (req, res) =>{
     try {
@@ -25,11 +40,38 @@ const getMovieById = async (req, res) =>{
 }
 
 const createMovie = async  (req, res) =>{
-    const movie = await prisma.movie.create({
-        data:req.body
-    })
-    console.log(req.body)
-    res.status(201).json({message:"Record Successfully created", movie})
+    try {
+        const uploadedImage = await cloudinary.v2.uploader.upload(req.file.path);
+        const {title,description,releaseYear,genre,director,rating,imageUrl} = req.body;
+        
+
+        if (!title || !description || !releaseYear) {
+            return res.status(400).json({ message: "Title, description, and release year are required fields" });
+        }
+
+        const movie = await prisma.movie.create({
+            data:{
+                title: title.trim(),
+                description: description.trim(),
+                releaseYear:releaseYear,
+                genre:genre,
+                director:director,
+                rating:parseInt(rating),
+                imageUrl:uploadedImage.secure_url
+            }
+        })
+    
+        // delete file from uploads folder
+        unlink(req.file.path,(err)=>{
+            if(err) return res.json('failed to delete file');
+        });
+
+        res.status(201).json({message:"Record Successfully created", movie})
+        
+    } catch (error) {
+        console.log(error)
+    }
+    
 }
 
 const updateMovie = async (req, res) =>{
@@ -73,4 +115,4 @@ const deleteMovieById = async (req, res) =>{
 
 
 
-module.exports = {getAllMovies, getMovieById,createMovie,updateMovie,deleteMovieById}
+module.exports = {getAllMovies, getMovieById,createMovie,updateMovie,deleteMovieById,upload}
--- v1/routes/movie.js
+++ v1/routes/movie.js
@@ -2,14 +2,14 @@ const express = require('express');
 const route = express.Router();
 
 const {authenticate}= require('../../middleware/authenticate')
-const movieController = require('../controllers/movieController')
+const {getAllMovies, getMovieById,createMovie,updateMovie,deleteMovieById,upload} = require('../controllers/movieController')
 
 
-route.get('/', movieController.getAllMovies);
-route.get('/:id',movieController.getMovieById);
-route.post('/', authenticate, movieController.createMovie);
-route.put('/:id', authenticate, movieController.updateMovie);
-route.delete('/:id',authenticate, movieController.deleteMovieById);
+route.get('/',getAllMovies);
+route.get('/:id',getMovieById);
+route.post('/', authenticate,upload.single('imageUrl'), createMovie);
+route.put('/:id', authenticate, updateMovie);
+route.delete('/:id',authenticate,deleteMovieById);
 
 
 
