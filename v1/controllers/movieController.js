5b9ebb8b0dd0af7ecb13ad4e58918f2943076bfd
Famba Abdulnassiir Siraji <57795345+FambaAbdulnassiirSiraji@users.noreply.github.com>
2023-11-28 00:51:11 +0300

    add try catch and handle errors on movie and usercontroller

--- v1/controllers/movieController.js
+++ v1/controllers/movieController.js
@@ -1,152 +1,125 @@
-const {PrismaClient} = require('@prisma/client')
+const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 const dotenv = require('dotenv');
 dotenv.config();
 const multer = require('multer');
-const {unlink} = require('fs');
-const cloudinary = require('../../config/cloudinary')
-const {movieValidation} = require('../../lib/validations')
+const { unlink } = require('fs');
+const cloudinary = require('../../config/cloudinary');
+const { movieValidation } = require('../../lib/validations');
 
-
-const upload = multer({dest: "uploads/"});
+const upload = multer({ dest: 'uploads/' });
 
 const postImage = async (req, res) => {
-    try {        
-        
-        const imageUrl = await cloudinary.v2.uploader.upload(req.file.path); 
-
+    try {
+        const imageUrl = await cloudinary.v2.uploader.upload(req.file.path);
         res.status(200).json({ imageUrl });
-
-         // delete file from uploads folder
-        unlink(req.file.path,(err)=>{
-            if(err) return res.json('failed to delete file');
+        unlink(req.file.path, (err) => {
+            if (err) res.json('Failed to delete file');
         });
-
     } catch (error) {
         res.status(500).json({ error: 'Failed to upload image' });
     }
 };
 
-
-
-const getAllMovies = async (req, res) =>{
+const getAllMovies = async (req, res) => {
     try {
         const movies = await prisma.movie.findMany({
-            include:{
-                comments:{},
-                genres:{
-                    select:{genre:true}
-                },
-                orders:{}
-
+            include: {
+                comments: {},
+                genres: { select: { genre: true } },
+                orders: {}
             }
-        })
-        res.status(200).json(movies)        
+        });
+        res.status(200).json(movies);
     } catch (error) {
-        res.status(400).json({message:'Bad Request'})        
+        res.status(400).json({ message: 'Bad Request' });
     }
-   
-}
+};
 
-const getMovieById = async (req, res) =>{
-    const id = parseInt(req.params.id)
-    const movie = await prisma.movie.findUnique({
-        where:{
-            id
-        },
-        include:{
-            comments:{},
-            genres:{
-                select:{genre:true}
-            },
-            orders:{}
-
-        }
-    })
-    if(movie) return res.status(200).json({movie})
-    return  res.status(401).json({message:`No movie with that id ${id}`})
-    
-}
-
-const createMovie = async  (req, res) =>{
-
+const getMovieById = async (req, res) => {
+    const id = parseInt(req.params.id);
     try {
-        
-        const {title,description,releaseYear,genre,director,rating,imageUrl,trailorUrl} = req.body;
+        const movie = await prisma.movie.findUnique({
+            where: { id },
+            include: {
+                comments: {},
+                genres: { select: { genre: true } },
+                orders: {}
+            }
+        });
+        if (movie) return res.status(200).json({ movie });
+        return res.status(404).json({ message: `No movie with id ${id}` });
+    } catch (error) {
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
 
-        // Validate data before it is added to the database
-        const {error} = movieValidation(req.body);
-        if(error) {
-            return res.status(400).json({ message:error.details[0].message});
-        }
-        
+const createMovie = async (req, res) => {
+    try {
+        const { title, description, releaseYear, genre, director, rating, imageUrl, trailorUrl } = req.body;
+        const { error } = movieValidation(req.body);
+
+        if (error) return res.status(400).json({ message: error.details[0].message });
 
         if (!title || !description || !releaseYear) {
-            return res.status(400).json({ message: "Title, description, and release year are required fields" });
+            return res.status(400).json({ message: 'Title, description, and release year are required fields' });
         }
 
         const movie = await prisma.movie.create({
-            data:{
+            data: {
                 title: title.trim(),
                 description: description.trim(),
-                releaseYear:parseInt(releaseYear),
-                genre:genre,
-                director:director,
-                rating:parseInt(rating),
-                imageUrl:imageUrl,
-                // trailorUrl:trailorUrl.trim()
+                releaseYear: parseInt(releaseYear),
+                genre,
+                director,
+                rating: parseInt(rating),
+                imageUrl,
+                // trailorUrl: trailorUrl.trim()
             }
-        })
-    
-       
-        res.status(201).json({message:"Record Successfully created", movie})
-        
+        });
+
+        res.status(201).json({ message: 'Record Successfully created', movie });
     } catch (error) {
-        console.log(error)
+        console.error('Error occurred:', error);
+        res.status(500).json({ error: 'Failed to create movie' });
     }
-    
-}
+};
 
-const updateMovie = async (req, res) =>{
-    const id = parseInt(req.params.id)
-    const movie = await prisma.movie.findUnique({
-        where:{
-            id
-        }
-    })
-    if(movie){
-        const updateMovie = await prisma.movie.update({
-            where:{
-                id
-            },
-            data:req.body
-        })
-        return  res.status(200).json(updateMovie)
-    } else
-    return  res.status(401).json({message:`No movie with that id ${id}`})
-    
-}
+const updateMovie = async (req, res) => {
+    const id = parseInt(req.params.id);
+    try {
+        const movie = await prisma.movie.findUnique({ where: { id } });
 
-const deleteMovieById = async (req, res) =>{
-    const id = parseInt(req.params.id)
-    const movie = await prisma.movie.findUnique({
-        where:{
-            id
-        }
-    })
-    if(movie){
-        const deleteMovie = await prisma.movie.delete({
-            where:{
-                id
-            }
-        })
-        return  res.status(200).json({message:`Record Successfully Deleted`})
-    } else
-    return  res.status(401).json({message:`No movie with that id ${id}`})
-    
-}
+        if (!movie) return res.status(404).json({ message: `No movie with id ${id}` });
 
+        const updatedMovie = await prisma.movie.update({
+            where: { id },
+            data: req.body
+        });
 
+        return res.status(200).json(updatedMovie);
+    } catch (error) {
+        console.error('Error occurred:', error);
+        res.status(500).json({ error: 'Failed to update movie' });
+    }
+};
 
-module.exports = {getAllMovies, getMovieById,createMovie,updateMovie,deleteMovieById,upload,postImage }
+const deleteMovieById = async (req, res) => {
+    const id = parseInt(req.params.id);
+    try {
+        const movie = await prisma.movie.findUnique({ where: { id } });
+
+        if (!movie) return res.status(404).json({ message: `No movie with id ${id}` });
+
+        await prisma.movie.delete({ where: { id } });
+
+        return res.status(200).json({ message: 'Record Successfully Deleted' });
+    } catch (error) {
+        console.error('Error occurred:', error);
+        res.status(500).json({ error: 'Failed to delete movie' });
+    }
+};
+
+module.exports = { getAllMovies, getMovieById, createMovie, updateMovie, deleteMovieById, upload, postImage };
+
--- v1/controllers/userController.js
+++ v1/controllers/userController.js
@@ -1,110 +1,107 @@
 const bcrypt = require('bcrypt');
-const { PrismaClient } = require('@prisma/client')
+const { PrismaClient } = require('@prisma/client');
 
-const prisma = new PrismaClient
-const { userValidation } = require('../../lib/validations')
+const prisma = new PrismaClient();
+const { userValidation } = require('../../lib/validations');
 
 const userSignup = async (req, res) => {
-
     try {
-        const { userName, email, password, role } = req.body
+        const { userName, email, password, role } = req.body;
 
-        // Validate data before it is added to the database
         const { error } = userValidation(req.body);
         if (error) {
             return res.status(400).json({ message: error.details[0].message });
         }
 
         const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt)
+        const hashedPassword = await bcrypt.hash(password, salt);
 
         const userExists = await prisma.user.findUnique({
             where: {
                 email
             }
-        })
-        if (userExists) return res.status(422).json({ message: "User already exists" })
+        });
+
+        if (userExists) {
+            return res.status(422).json({ message: 'User already exists' });
+        }
 
         const user = await prisma.user.create({
-
             data: {
                 userName: userName.trim(),
                 email: email.trim(),
                 password: hashedPassword,
                 // role: role.trim()
             }
+        });
 
-        })
-        res.status(201).json(user)
-
+        res.status(201).json(user);
     } catch (error) {
-        console.log(error);
-        res.status(404).json({ message: "failed to register user" })
-
+        console.error('Error occurred:', error);
+        res.status(500).json({ message: 'Failed to register user' });
     }
-
-}
+};
 
 const getAllUsers = async (req, res, next) => {
     try {
-        const users = await prisma.user.findMany({})
-        res.status(200).json(users)
+        const users = await prisma.user.findMany({});
+        res.status(200).json(users);
     } catch (error) {
-        next(error)
+        next(error);
     }
-}
+};
 
 const getUserById = async (req, res) => {
-    const id = parseInt(req.params.id)
-    const user = await prisma.user.findUnique({
-        where: {
-            id
-        },
-
-    })
-    if (user) return res.status(200).json({ user })
-    return res.status(401).json({ message: `No User with that id ${id} is Found` })
-
-}
+    const id = parseInt(req.params.id);
+    try {
+        const user = await prisma.user.findUnique({
+            where: { id }
+        });
+        if (user) {
+            return res.status(200).json({ user });
+        }
+        return res.status(404).json({ message: `No user with id ${id} found` });
+    } catch (error) {
+        console.error('Error occurred:', error);
+        res.status(500).json({ message: 'Failed to fetch user' });
+    }
+};
 
 const updateUser = async (req, res) => {
-    const id = parseInt(req.params.id)
-    const user = await prisma.user.findUnique({
-        where: {
-            id
+    const id = parseInt(req.params.id);
+    try {
+        const user = await prisma.user.findUnique({
+            where: { id }
+        });
+        if (user) {
+            const updatedUser = await prisma.user.update({
+                where: { id },
+                data: req.body
+            });
+            return res.status(200).json(updatedUser);
         }
-    })
-    if (user) {
-        const updatedUser = await prisma.user.update({
-            where: {
-                id
-            },
-            data: req.body
-        })
-        return res.status(200).json(updatedUser)
-    } else
-        return res.status(401).json({ message: `No User with that id ${id} was found` })
-
-}
+        return res.status(404).json({ message: `No user with id ${id} found` });
+    } catch (error) {
+        console.error('Error occurred:', error);
+        res.status(500).json({ message: 'Failed to update user' });
+    }
+};
 
 const deleteUserById = async (req, res) => {
-    const id = parseInt(req.params.id)
-    const user = await prisma.user.findUnique({
-        where: {
-            id
+    const id = parseInt(req.params.id);
+    try {
+        const user = await prisma.user.findUnique({
+            where: { id }
+        });
+        if (user) {
+            await prisma.user.delete({ where: { id } });
+            return res.status(200).json({ message: 'Record successfully deleted' });
         }
-    })
-    if (user) {
-        const deleteduser = await prisma.user.delete({
-            where: {
-                id
-            }
-        })
-        return res.status(200).json({ message: `Record Successfully Deleted` })
-    } else
-        return res.status(401).json({ message: `No User with that id ${id} was Found` })
+        return res.status(404).json({ message: `No user with id ${id} found` });
+    } catch (error) {
+        console.error('Error occurred:', error);
+        res.status(500).json({ message: 'Failed to delete user' });
+    }
+};
 
-}
-
-
-module.exports = { userSignup, getAllUsers, getUserById, updateUser, deleteUserById }
+module.exports = { userSignup, getAllUsers, getUserById, updateUser, deleteUserById };
